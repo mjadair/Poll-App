5e58c8b9bc66886fccda063afff7c76e8b027c18
Michael Adair <mjadair91@gmail.com>
2020-04-11 16:47:55 +0100

    fixed models and made migrations

--- poll/polls/models.py
+++ poll/polls/models.py
@@ -3,12 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-class Question(models.model):
-    question_text = models.charField(max_length=200)
+#The first positional argument of each of the fields can be changed to a human readable format.
+#This is the case with Question in the Choice model. 
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
-class Choice(models.model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.charField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
